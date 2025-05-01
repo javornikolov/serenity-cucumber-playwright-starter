665629444c9358263164205664b673c42a3c27a3
Yavor Nikolov <nikolov.javor@gmail.com>
2025-05-01 14:21:20 +0300

    CucumberTestSuite refinement
    Long output, explicit glue

--- src/test/java/starter/CucumberTestSuite.java
+++ src/test/java/starter/CucumberTestSuite.java
@@ -7,9 +7,18 @@ import org.junit.platform.suite.api.Suite;
 
 import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 
+import io.cucumber.junit.platform.engine.Constants;
+
 @Suite
 @IncludeEngines("cucumber")
 @SelectClasspathResource("/features")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "io.cucumber.core.plugin.SerenityReporterParallel,pretty,timeline:build/test-results/timeline")
+@ConfigurationParameter(key = Constants.GLUE_PROPERTY_NAME, value = "starter")
+@ConfigurationParameter(
+    key = PLUGIN_PROPERTY_NAME,
+    value = "io.cucumber.core.plugin.SerenityReporterParallel,pretty,timeline:build/reports/cucumber/timeline"
+)
+@ConfigurationParameter(key = "cucumber.filter.tags", value = "not @disabled")
+@ConfigurationParameter(key = "cucumber.publish.quiet", value = "false")
+@ConfigurationParameter(key = "cucumber.junit-platform.naming-strategy", value = "long")
 public class CucumberTestSuite {
 }
